c42ddc810dda24b4b818bc865ecbb10364b4551d
Lílian Vieira <lilianv528@gmail.com>
2024-02-06 13:59:31 -0300

    :tada: feat: controllers editar excluir

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -34,6 +34,83 @@ namespace sistemaProdutos.Controllers
       }
       return View(produto);
     }
+
+    public async Task<IActionResult> Editar(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var product = await _context.Produtos.FindAsync(id);
+      if (product == null)
+      {
+        return NotFound();
+      }
+      return View(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Editar(int id, [Bind("Id,Nome,Valor,Codigo,Estoque")] Produto produto)
+    {
+      if (id != produto.Id)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(produto);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!ProductExists(produto.Id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(produto);
+    }
+
+    public async Task<IActionResult> Excluir(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var product = await _context.Produtos
+          .FirstOrDefaultAsync(m => m.Id == id);
+      if (product == null)
+      {
+        return NotFound();
+      }
+
+      return View(product);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> ConfirmarExclusao(int id)
+    {
+      var product = await _context.Produtos.FindAsync(id);
+      _context.Produtos.Remove(product);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    private bool ProductExists(int id)
+    {
+      return _context.Produtos.Any(e => e.Id == id);
+    }
   }
 }
 
